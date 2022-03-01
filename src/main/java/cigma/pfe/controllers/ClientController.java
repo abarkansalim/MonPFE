27abeedf89bde85544995ce2b8a300cf67f2d00a
USER <abarkansalim@gmail.com>
2022-03-01 16:38:15 +0100

    'Seance V. Injection des dépendances par Setter'

--- src/main/java/cigma/pfe/controllers/ClientController.java
+++ src/main/java/cigma/pfe/controllers/ClientController.java
@@ -2,10 +2,13 @@ package cigma.pfe.controllers;
 
 import cigma.pfe.models.Client;
 import cigma.pfe.services.ClientService;
-import cigma.pfe.services.ClientServiceImpl;
 
 public class ClientController {
-    ClientService clientService = new ClientServiceImpl();
+    ClientService clientService;
+    public void setClientService(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
     public Client save(Client c ){
         System.out.println("ClientController level...");
         return clientService.save(c);
